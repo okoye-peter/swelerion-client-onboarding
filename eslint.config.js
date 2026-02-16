7c0118ca46629310ab8ba359c020e7d153946228
Okoye Peter <okoyepeter039@gmail.com>
2026-02-16 14:36:39 +0100

    chore: Remove legacy source code, build configurations, and assets, with minor updates to ESLint and TypeScript configurations.

--- eslint.config.js
+++ eslint.config.js
@@ -6,7 +6,7 @@ import tseslint from 'typescript-eslint'
 import { defineConfig, globalIgnores } from 'eslint/config'
 
 export default defineConfig([
-    { ignores: ['dist', '_src_legacy', 'vite.config.ts', '_src_legacy_vite'] },
+    { ignores: ['dist'] },
     {
         files: ['**/*.{ts,tsx}'],
         extends: [
